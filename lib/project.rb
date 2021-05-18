a93b743c0cc87b0c3a888fbbb123697c837b5e0f
Shasha Yan <shashayan@gmail.com>
2021-05-18 01:59:37 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+
+  attr_reader :title, :backers 
+
+  def initialize
+    @title = title
+    @backers = [ ]
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    Backer.project << self
+  end
+
+end 
